352d02d3cb77087edeb744d24058647e8143f969
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-11-09 14:42:57 +0200

    app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  render() {
+    return <input type="text" name="name" required />;
+  }
+}
+
+export default App;
